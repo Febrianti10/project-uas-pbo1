6cda0b00974e4a82f1b13a579b83c9507daccc7f
kharizma <h1101241050@student.untan.ac.id>
2025-11-22 07:14:09 +0700

    update

--- views/layanan.php
+++ views/layanan.php
@@ -126,7 +126,7 @@ $layananTambahan = [
                 </div>
 
                 <!-- Modal Edit Paket -->
-                <div class="modal fade" id="<?= $modalId; ?>" tabindex="-1">
+                <div class="modal" id="<?= $modalId; ?>" tabindex="-1">
                     <div class="modal-dialog modal-dialog-centered">
                         <form class="modal-content"
                               method="post"
