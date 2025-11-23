5a87c9c3871c593b0038c967b9842d21b3d8b4eb
bigqmope <adella.sweeta@gmail.com>
2025-11-23 18:25:22 +0700

    Update database.php

--- config/database.php
+++ config/database.php
@@ -2,7 +2,7 @@
 return [
     'host'     => getenv('MYSQLHOST') ?: 'localhost',  // ← ini fallback ke localhost
     'port'     => getenv('MYSQLPORT') ?: '3306',       
-    'dbname'   => getenv('MYSQLDATABASE') ?: 'mvc_db', // ← nama database lokal
+    'dbname'   => getenv('MYSQLDATABASE') ?: 'sip_hewan', // ← nama database lokal
     'username' => getenv('MYSQLUSER') ?: 'root',       // ← user lokal
     'password' => getenv('MYSQLPASSWORD') ?: '',       // ← password kosong (lokal)
     'charset'  => 'utf8mb4'
