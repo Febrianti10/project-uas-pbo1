5d49d687d0224eb537fa101a15deec6b55868ba7
Febrianti10 <fbrntkhmrh@gmail.com>
2025-11-27 17:12:49 +0700

    Persiapan deploy render

--- config/database.php
+++ config/database.php
@@ -1,5 +1,4 @@
 <?php
-
 class Database
 {
     private static $instance = null;
@@ -7,25 +6,24 @@ class Database
 
     private function __construct()
     {
-        // KREDENSIAL DATABASE
-        $host = getenv('MYSQL_HOST') ?: 'localhost'; 
-        $port = getenv('MYSQL_PORT') ?: '3306'; 
-        $dbname = getenv('MYSQL_DATABASE') ?: 'sip_hewan'; // Database Anda: sip_hewan
-        $user = getenv('MYSQL_USER') ?: 'root'; 
-        $pass = getenv('MYSQL_PASSWORD') ?: ''; 
-        $charset = 'utf8mb4';
-        
-        $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=$charset";
+        // Render menyediakan database variables lewat Environment Variables
+        $host = getenv('DB_HOST') ?: 'localhost';
+        $port = getenv('DB_PORT') ?: '3306';
+        $dbname = getenv('DB_NAME') ?: 'sip_hewan';
+        $user = getenv('DB_USER') ?: 'root';
+        $pass = getenv('DB_PASSWORD') ?: '';
+
+        $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4";
         $options = [
-            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, 
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,     
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::ATTR_EMULATE_PREPARES   => false,
         ];
 
         try {
             $this->pdo = new PDO($dsn, $user, $pass, $options);
         } catch (\PDOException $e) {
-            die("Koneksi Database Gagal: " . $e->getMessage()); 
+            die("Koneksi Database Gagal: " . $e->getMessage());
         }
     }
 
